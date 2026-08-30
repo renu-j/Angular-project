643e17fca58cb5d5e60e4f484e9711bd44de0ded
rejoshi <rejoshi@ICS-LT-G2X1HK3>
2026-08-30 18:50:12 +0545

    day 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,46 +15,38 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.html',
 })
 export class App {
- 
-  @ViewChild('TestCheckboxTemp')
-  tempCheckbox!: ElementRef<HTMLInputElement>;
- 
-  protected readonly title = signal('hrm');
- 
-  header: string = "Second Line Data Binding";
- 
-  a: number = 5;
+  @ViewChild('TestCheckboxTemp') tempCheckbox: ElementRef<HTMLInputElement> | undefined;
+  protected readonly title = signal('HRM');
+
+  header: string = "App Component";
+
+  a: number = 5.1;
   b: number = 7;
- 
-  isChecked: boolean = false;
- 
-  dothis: string = this.isChecked ? "True" : "False";
- 
-  isChecked2 = signal(true);
- 
- 
-  // Called when checkbox state changes
-  changeCheckbox(): void {
- 
+
+  secondTitle: string = "The user is active";
+  secondTitl2 = signal("The user is active");
+
+  isChecked: boolean = true;
+  isChecked2 = signal<boolean>(true);
+
+  dothis: string = this.isChecked ? "True" : "false"; // ischecked
+
+  changeCheckbox() {
+    // this.isChecked2.set(false);
+
+    // this.isChecked = !this.isChecked;
+    // // this.isChecked = value;
+    // this.dothis = this.isChecked ? "True" : "false"
+    // return "";
+    // this.isChecked = <boolean>this.tempCheckbox?.nativeElement.checked;
     console.log("Reached");
- 
-    console.log("Checkbox value:", this.isChecked);
- 
   }
- 
- 
-  // Example using ViewChild
-  getCheckboxValue(): void {
- 
-    if (this.tempCheckbox) {
- 
-      console.log(
-        "Checked:",
-        this.tempCheckbox.nativeElement.checked
-      );
- 
-    }
- 
+
+  handleChildEvent(value: string) {
+    this.header = value;
   }
- 
 }
+
+// One way Binding
+
+// Two way Binding
