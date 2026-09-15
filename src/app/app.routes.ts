7076e43713e4f8549858d9b252fda947af2d9e37
rejoshi <rejoshi@ICS-LT-G2X1HK3>
2026-09-15 07:53:06 +0545

    implementation of teh guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,30 @@
 import { Routes } from '@angular/router';
-
+import { authGuard, contactGuard } from './guards/common-guard';
+ 
 export const routes: Routes = [
     {
         path: 'dashboard',
         loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard),
+        canActivate: [authGuard]
     },
     {
         path: 'contact',
         loadComponent: () => import('./contact/contact').then(m => m.Contact),
+        canActivate: [authGuard, contactGuard],
+        // canActivateChild:
     },
     {
         path: 'login',
         loadComponent: () => import('./login/login').then(m => m.Login)
-    }
+    },
+    {
+        path: 'denied',
+        loadComponent: () => import('./denied/denied').then(m => m.Denied)
+    },
+   {
+  path: 'state',
+  loadComponent: () =>
+    import('./state-management/state-management')
+      .then(m => m.StateManagement)
+}
 ];
--- src/app/state-management/state-management.ts
+++ src/app/state-management/state-management.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-state-management',
+  imports: [],
   styleUrl: './state-management.scss',
   templateUrl: './state-management.html',
 })
-export class StateManagement {}
+export class StateManagement {
+
+  name: string = 'Ram';
+  displayName: string = '';
+
+  changeDisplayName() {
+    this.displayName = 'Hello, ' + this.name;
+  }
+
+  nameSignal = signal('Ram');
+
+  displayNameSignal = computed(() => {
+    return 'Hello, ' + this.nameSignal();
+  });
+
+    constructor(){
+    effect(()=>{
+      console.log("hello from console," + this.nameSignal());
+    } 
+  }
+  //observable/rjxs (tarditional industry standard)
+  name$ =new BehaviourSubject<string>("Ram");
+  displayName$= this.names$.pipe(
+    map(()=> "hello",+this,name$)
+  );
+  changeName(value:string){
+    this.names$.next(value);// thsi is the traditional way 
+  }
+}
